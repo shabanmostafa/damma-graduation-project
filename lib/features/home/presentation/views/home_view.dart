603e93e4baf06df6a5208802251fb294009a1baf
sm175075@gmail.com <YourEmail>
2025-04-12 17:04:53 +0200

    add icons to bottom navigation bar

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,30 +1,79 @@
-import 'package:damma_project/features/home/presentation/widgets/home_view_body.dart';
+import 'package:damma_project/core/utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:damma_project/features/home/presentation/widgets/home_view_body.dart';
 
 import '../../../../core/utils/app_colors.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    const HomeViewBody(),
+    const Center(child: Text("صفحة البحث")),
+    const Center(child: Text("صفحة الأصدقاء")),
+    const Center(child: Text("صفحتي")),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.whiteColor,
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 2,
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
         selectedItemColor: Colors.black,
         unselectedItemColor: Colors.grey,
         type: BottomNavigationBarType.fixed,
         backgroundColor: Colors.white,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "الرئيسية"),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: "البحث"),
-          BottomNavigationBarItem(icon: Icon(Icons.people), label: "الصداقة"),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: "بروفايلي"),
+        items: [
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              _currentIndex == 0
+                  ? Assets.svgHomeFilledBottomNavigationBar
+                  : Assets.svgHomeBottomNavigationBar,
+            ),
+            label: "الرئيسية",
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              _currentIndex == 1
+                  ? Assets.svgSearchFilledBottomNavigationBar
+                  : Assets.svgSearchBottomNavigationBar,
+            ),
+            label: "البحث",
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              _currentIndex == 2
+                  ? Assets.svgFriendFilledBottomNavigationBar
+                  : Assets.svgFriendNavigationBar,
+            ),
+            label: "الصداقة",
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              _currentIndex == 3
+                  ? Assets.svgProfileFilledBottomNavigationBar
+                  :Assets.svgProfileBottomNavigationBar,
+            ),
+            label: "بروفايلي",
+          ),
         ],
       ),
-      body: const SafeArea(
-        child: HomeViewBody(),
+      body: SafeArea(
+        child: _screens[_currentIndex],
       ),
     );
   }
